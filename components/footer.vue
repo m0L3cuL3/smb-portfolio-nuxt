a5681beeb617ea72845948725d5efd02f83b2033
Sean Baang <dev.seanbaang@gmail.com>
2023-05-25 15:09:29 +0800

    feat: added components

---
+++ components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer :app="true">
+    <div class="d-flex justify-center align-center w-100">
+      <h1 class="text-overline">Powered by DigitalOcean.</h1>
+    </div>
+  </v-footer>
+</template>
