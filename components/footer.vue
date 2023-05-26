0799ae099263233386f0eedfb214607e944a06f2
Sean Baang <dev.seanbaang@gmail.com>
2023-05-26 13:22:23 +0800

    refactor: added new text

--- components/footer.vue
+++ components/footer.vue
@@ -1,7 +1,10 @@
 <template>
   <v-footer :app="true">
     <div class="d-flex justify-center align-center w-100">
-      <h1 class="text-overline">Powered by DigitalOcean.</h1>
+      <h1 class="text-overline font-weight-thin">
+        Powered by <span class="font-italic font-weight-black">Nuxt 3</span> &
+        <span class="font-italic font-weight-black">DigitalOcean</span>.
+      </h1>
     </div>
   </v-footer>
 </template>
