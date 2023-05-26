5ed77738a2672af6034d8936dada4dace6a19fc9
Sean Baang <dev.seanbaang@gmail.com>
2023-05-26 13:24:17 +0800

    config: removed eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@invictus.codes/nuxt-vuetify',
-        '@hypernym/nuxt-anime',
-        ['@nuxtjs/eslint-module', {
-            include: ['*/{js,jsx,ts,tsx,vue}']
-        }]
-    ],
-    vuetify: {
-        /* vuetify options */
-        vuetifyOptions: {
-            // @TODO: list all vuetify options
-        },
-        moduleOptions: {
-            /* nuxt-vuetify module options */
-            treeshaking: true,
-            useIconCDN: true,
-            /* vite-plugin-vuetify options */
-            styles: true,
-            autoImport: true,
-        }
-    }
-})
+  modules: ["@invictus.codes/nuxt-vuetify", "@hypernym/nuxt-anime"],
+  vuetify: {
+    /* vuetify options */
+    vuetifyOptions: {
+      // @TODO: list all vuetify options
+    },
+    moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: true,
+      useIconCDN: true,
+      /* vite-plugin-vuetify options */
+      styles: true,
+      autoImport: true,
+    },
+  },
+});
