50de13f6f886610c0aac16aee949dc3420eeffcc
Sean Baang <dev.seanbaang@gmail.com>
2023-05-26 13:24:06 +0800

    chore: added development indicator, will disable once development is finished

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
   <v-main>
+    <v-system-bar window color="orange-lighten-1">
+      <v-spacer></v-spacer>
+      <h1 class="text-overline">Currently in development</h1>
+      <v-spacer></v-spacer>
+    </v-system-bar>
     <Header />
     <slot />
     <Footer />
