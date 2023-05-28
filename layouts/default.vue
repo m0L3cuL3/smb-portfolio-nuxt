8ef3df3d68cf40bff9cc9820a8afcb0f3831a7c5
Sean Baang <dev.seanbaang@gmail.com>
2023-05-28 21:35:54 +0800

    chore: moved `v-app` to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-main>
+  <v-app theme="dark">
     <v-system-bar window color="orange-lighten-1">
       <v-spacer></v-spacer>
       <h1 class="text-overline">Currently in development</h1>
       <v-spacer></v-spacer>
     </v-system-bar>
     <Header />
-    <slot />
+    <v-main>
+      <slot />
+    </v-main>
     <Footer />
-  </v-main>
+  </v-app>
 </template>
