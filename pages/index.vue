bc8de360e15fca9a89ed9dbd12db563f8cb9a103
Sean Baang <dev.seanbaang@gmail.com>
2023-05-28 21:36:37 +0800

    refactor: wrap section components with `div`

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,14 @@
   <Head>
     <Title>Home</Title>
   </Head>
-  <div class="w-100 h-100">
-    <SectionMain />
+  <div class="d-flex flex-column">
+    <div
+      class="bg-transparent d-flex flex-column justify-center align-center h-screen"
+    >
+      <SectionMain />
+    </div>
+    <div class="d-flex flex-column justify-center bg-grey-darken-4 py-12">
+      <SectionSkills />
+    </div>
   </div>
 </template>
