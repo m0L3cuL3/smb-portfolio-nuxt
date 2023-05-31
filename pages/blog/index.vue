cabb3b347275e9d8449419063c3fb3e45f1ec4de
Sean Baang <dev.seanbaang@gmail.com>
2023-05-31 13:24:37 +0800

    feat: added template for blog posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,7 +2,73 @@
   <Head>
     <Title>Blog</Title>
   </Head>
-  <div class="d-flex flex-column justify-center">
-    <h1>Blog Posts</h1>
+  <div class="d-flex flex-column justify-center align-center">
+    <div class="pa-4">
+      <h1>Blog Posts</h1>
+    </div>
+    <div
+      class="d-flex flex-wrap justify-space-evenly align-content-space-around"
+    >
+      <v-card width="600" class="ma-2 pa-2">
+        <v-card-title>Lorem Ipsum</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem quas iusto consequatur. Repellat labore earum dolore
+          est soluta repellendus consectetur molestias inventore minus sint
+          provident quos reiciendis, hic temporibus voluptatem!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="teal">Read More</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card width="600" class="ma-2 pa-2">
+        <v-card-title>Lorem Ipsum</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem quas iusto consequatur. Repellat labore earum dolore
+          est soluta repellendus consectetur molestias inventore minus sint
+          provident quos reiciendis, hic temporibus voluptatem!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="teal">Read More</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card width="600" class="ma-2 pa-2">
+        <v-card-title>Lorem Ipsum</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem quas iusto consequatur. Repellat labore earum dolore
+          est soluta repellendus consectetur molestias inventore minus sint
+          provident quos reiciendis, hic temporibus voluptatem!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="teal">Read More</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card width="600" class="ma-2 pa-2">
+        <v-card-title>Lorem Ipsum</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem quas iusto consequatur. Repellat labore earum dolore
+          est soluta repellendus consectetur molestias inventore minus sint
+          provident quos reiciendis, hic temporibus voluptatem!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="teal">Read More</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card width="600" class="ma-2 pa-2">
+        <v-card-title>Lorem Ipsum</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem quas iusto consequatur. Repellat labore earum dolore
+          est soluta repellendus consectetur molestias inventore minus sint
+          provident quos reiciendis, hic temporibus voluptatem!
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="teal">Read More</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </div>
 </template>
