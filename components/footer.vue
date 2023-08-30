a139ea61e779d6050aec9d1b8a1701ee5b149834
Sean Baang <dev.seanbaang@gmail.com>
2023-08-31 01:11:13 +0800

    refactor: added new style class

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer :app="true">
+  <v-footer class="d-flex flex-column">
     <div class="d-flex justify-center align-center w-100">
       <h1 class="text-overline font-weight-thin">
         Powered by <span class="font-italic font-weight-black">Nuxt 3</span> &
