402109ec1e866fba3f6fb5537444dacec588c276
Sean Baang <dev.seanbaang@gmail.com>
2023-08-31 01:09:47 +0800

    refactor: index file only now contains custom section tags

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,6 @@
   <Head>
     <Title>Home</Title>
   </Head>
-  <div class="d-flex flex-column">
-    <div
-      class="bg-transparent d-flex flex-column justify-center align-center h-screen"
-    >
-      <SectionMain />
-    </div>
-  </div>
+  <SectionMain />
+  <SectionSkills />
 </template>
